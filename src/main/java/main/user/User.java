3dc8562ab19fbbb9bf762738922d26dc5b7d25eb
Maria Eduarda Spiess <maria.spiess@senior.com.br>
2019-10-30 10:42:20 -0300

    usuario

---
+++ src/main/java/main/user/User.java
@@ -0,0 +1,29 @@
+package main.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Component
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Lob
+    @Column(name = "password", nullable = false)
+    private String password;
+}
---
+++ src/main/java/main/user/UserRepository.java
@@ -0,0 +1,7 @@
+package main.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsernameAndPassword(String username, String password);
+}
